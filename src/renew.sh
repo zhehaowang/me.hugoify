4546df439f5daedad303ab3c49dc0723ad341228
Zhehao Wang <wangzhehao410305@gmail.com>
2020-04-24 03:01:57 +0000

    renew: add cert renew script

---
+++ src/renew.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/zhehao
+./certbot-auto renew --pre-hook "service haproxy stop" --post-hook "service haproxy start"
+DOMAIN='zhehao.me' sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
