83c48ebe2fef82559bb3ceb6af555ddf981efb6e
Zhehao Wang <wangzhehao410305@gmail.com>
2021-04-01 23:59:15 -0400

    renew.sh: reflect new domain

--- src/renew.sh
+++ src/renew.sh
@@ -2,4 +2,4 @@
 
 cd /home/pi
 certbot renew --pre-hook "service haproxy stop" --post-hook "service haproxy start"
-DOMAIN='zhehao.me' sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
+DOMAIN='zhehaopi.ddns.net' sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
